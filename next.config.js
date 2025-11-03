43fcfb394614802a95692d434a66ff75cdcc6794
Ohpeter <158809864+Ohpeter@users.noreply.github.com>
2025-11-03 21:35:16 +0900

    Modify next.config.js for static export settings
    Updated Next.js configuration for static export and added base path.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+// next.config.js
+const repo = 'lookit-kakao-preview'
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
+  output: 'export',         // 정적 내보내기
+  trailingSlash: true,      // /simulator/ 로 내보냄
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
 }
+
 module.exports = nextConfig
